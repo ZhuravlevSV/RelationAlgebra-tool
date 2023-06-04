dd6f6589533ab79ef1602df79de906dc14fc6dbb
Semen Zhuravlev <zhuravlev.more@gmail.com>
2023-06-04 21:54:42 +0200

    Added export files

---
+++ src/file_export.h
@@ -0,0 +1,54 @@
+#ifndef FILE_EXPORT_H
+#define FILE_EXPORT_H
+
+#include "config.h"
+#include "tables.h"
+
+using namespace std;
+
+class file_export_c{
+public:
+
+    file_export_c();
+
+    file_export_c(const string& new_file_name, shared_ptr<table_c> new_table_src);
+
+    virtual bool export() = 0;
+
+protected:
+
+    string file_name;
+    shared_ptr<table_c> table_src;
+};
+
+class file_CSV_c : public file_export_c{
+public:
+
+    file_CSV_c();
+
+    file_CSV_c(const string& new_file_name, shared_ptr<table_c> new_table_src);
+
+    bool export() override;
+};
+
+class file_XML_c : public file_export_c{
+public:
+
+    file_XML_c();
+
+    file_XML_c(const string& new_file_name, shared_ptr<table_c> new_table_src);
+
+    bool export() override;
+};
+
+class file_JSON_c : public file_export_c{
+public:
+
+    file_JSON_c();
+
+    file_JSON_c(const string& new_file_name, shared_ptr<table_c> new_table_src);
+
+    bool export() override;
+};
+
+#endif /* FILE_EXPORT_H */
