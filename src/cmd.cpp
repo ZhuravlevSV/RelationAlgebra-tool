d9baf864c4ca9191d7450ff460dba97a591bf221
Semen Zhuravlev <zhuravlev.more@gmail.com>
2023-06-06 13:58:49 +0200

    add projection sql

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -886,6 +886,8 @@ bool cmd_sql_rename_c::run(tables_c& tables){
             return true;
         }
     }
+
+    cout << WARNING_TEXT << ": " << UNDERLINE_FONT << "SQL RENAME" << RESET_FONT << " aborted" << endl;
     return false;
 }
 
@@ -897,9 +899,25 @@ cmd_sql_projection_c::cmd_sql_projection_c(const string& new_cmd_text)
     : cmd_c(new_cmd_text) {}
 
 bool cmd_sql_projection_c::run(tables_c& tables){
-    cout << "SQL PROJECTION" << endl;
+    regex projection_regex("\\s*PROJECTION\\s*(\\w+)\\s*OF\\s*(\\w+)\\s*\\[(\\w+(?:,\\s*\\w+)*)\\]\\s*");
+    match_results<string::const_iterator> match;
 
-    return true;
+    // compare request
+    if(regex_search(cmd_text, match, projection_regex) && match.size() == 4){
+        string table_name_dst = match[1];
+        string table_name_src = match[2];
+        string attributes = match[3];
+
+        // print SQL
+        cout << endl << "CREATE TABLE " << table_name_dst << " AS" << endl;
+        cout << "SELECT " << attributes << endl;
+        cout << "FROM " << table_name_src << ";" << endl << endl;
+        
+        return true;
+    }
+    
+    cout << WARNING_TEXT << ": " << UNDERLINE_FONT << "SQL PROJECTION" << RESET_FONT << " aborted" << endl;
+    return false;
 }
 
 // ==========================================
