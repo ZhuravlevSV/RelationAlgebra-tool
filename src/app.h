7bc4801725ffe11c5b8a9f39c71b5deb0f250800
Semen Zhuravlev <zhuravlev.more@gmail.com>
2023-06-07 17:40:56 +0200

    added apps polymorfismus

--- src/app.h
+++ src/app.h
@@ -9,17 +9,63 @@ using namespace std;
 class app_c{
 public:
 
-    void launch();
+    virtual ~app_c() = default;
+
+    virtual void launch() = 0;
 
 private:
 
-    void print_start();
+    virtual void print_start() = 0;
 
-    void print_finish();
+    virtual void print_finish() = 0;
 
-    void create_folders();
+    virtual void create_folders() = 0;
+
+protected:
 
     manager_c manager;
+
+};
+
+class app_launcher_c : public app_c {
+public:
+
+    void launch() override;
+
+private:
+
+    void print_start() override;
+
+    void print_finish() override;
+    
+    void create_folders() override;
+
+};
+
+class app_tester_c : public app_c {
+public:
+
+    void launch() override;
+
+private:
+
+    void print_start() override;
+
+    void print_finish() override;
+    
+    void create_folders() override;
+
+    void cmd_ok_work_ok(string cmd_text);
+
+    void cmd_er_work_er(string cmd_text);
+
+    void cmd_ok_work_er(string cmd_text);
+
+};
+
+class app_factory_c{
+public:
+    static unique_ptr<app_c> create_app(int argc, char* argv[]);
 };
 
 #endif /* APP_H */
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,11 @@
 #include "config.h"
 #include "app.h"
-#include "app_tester.h"
 
 
 int main(int argc, char* argv[]){
     
-    // auto test
-    if(argc == 2){
-        string argument = argv[1];
-        if(argument == "test"){
-            app_tester_c app_tester;
-            app_tester.launch();
-        }
-    }
-    
-    // application
-    app_c app;
-    app.launch();
+    unique_ptr<app_c> app = app_factory_c::create_app(argc, argv);
+    app->launch();
 
     return 0;
 }
