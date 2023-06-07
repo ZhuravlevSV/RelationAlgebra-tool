127822bc31a22f55f8461ec2e3490a3b8c4f9a49
Semen Zhuravlev <zhuravlev.more@gmail.com>
2023-06-07 22:05:42 +0200

    Added comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@
 
 int main(int argc, char* argv[]){
     
+    // Create and define an application
     unique_ptr<app_c> app = app_factory_c::create_app(argc, argv);
+
+    // Launch current application
     app->launch();
 
     return 0;
