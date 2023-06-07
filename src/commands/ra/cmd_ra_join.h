a4c17bb7217d12e097f559dceb061a273c29863b
Semen Zhuravlev <zhuravlev.more@gmail.com>
2023-06-07 22:26:49 +0200

    Added comments

--- src/commands/ra/cmd_ra_join.h
+++ src/commands/ra/cmd_ra_join.h
@@ -3,13 +3,20 @@
 
 #include "../../cmd.h"
 
+/// @brief Command class in RA format of type Join
 class cmd_ra_join_c : public cmd_c{
 public:
 
+    /// @brief Base constructor
     cmd_ra_join_c();
 
+    /// @brief Initial constructor with command text
+    /// @param new_cmd_text New command text
     cmd_ra_join_c(const string& new_cmd_text);
 
+    /// @brief Command Run Method
+    /// @param tables Existing tables
+    /// @return TRUE - if the command was executed correctly, FALSE - otherwise
     bool run(tables_c& tables) override;
 
 };
--- src/commands/ra/cmd_ra_print.h
+++ src/commands/ra/cmd_ra_print.h
@@ -3,13 +3,20 @@
 
 #include "../../cmd.h"
 
+/// @brief Command class in RA format of type Print
 class cmd_ra_print_c : public cmd_c{
 public:
 
+    /// @brief Base constructor
     cmd_ra_print_c();
 
+    /// @brief Initial constructor with command text
+    /// @param new_cmd_text New command text
     cmd_ra_print_c(const string& new_cmd_text);
 
+    /// @brief Command Run Method
+    /// @param tables Existing tables
+    /// @return TRUE - if the command was executed correctly, FALSE - otherwise
     bool run(tables_c& tables) override;
 
 };
